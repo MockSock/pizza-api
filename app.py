7d8f9a36753fddf64fe01c1d8789c5e2f348de1f
madden20202 <business.mocksock@gmail.com>
2022-07-25 20:50:27 -0500

    add route to pizza orders

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/orders')
+def getPizzaOrders():
+    return 'I will get pizza orders one day'
 
 if __name__ == "__main__":
     app.run()
