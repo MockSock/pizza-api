de4d97008c603e4a57b1d5b0c02dfc2fa6e3a528
madden20202 <business.mocksock@gmail.com>
2022-07-25 21:06:08 -0500

    add schema

--- app.py
+++ app.py
@@ -11,11 +11,16 @@ app.config['SQLALCHEMY_TRACK_NOTIFICATIONs'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-class PizzaOrder (db.Model):
+class PizzaOrder(db.Model):
     orderId = db.Column(db.Integer, primary_key=True)
     size = db.Column(db.String(500))
     crust = db.Column(db.String(500))
-    toppings = db.Column(db.String(500))
+    topping = db.Column(db.String(500))
+
+class PizzaOrderSChema(ma.Schema):
+    class Meta:
+        fields = ('orderId', 'size', 'crust', 'topping')
+
 
 @app.route('/')
 def hello_world():
